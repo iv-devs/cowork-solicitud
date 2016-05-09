0122b51fffab0d3f63a4b5278e59ab38b17d3f68
Cesar Enrique Encina Leon <ceel2004@hotmail.com>
2016-05-09 14:52:42 -0300

    Update index.js
    ADD Constraint to event and selection

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,39 +2,41 @@
 
 $(document).ready(function() {
 
-		$('#calendar').fullCalendar({
-			header: {
-				left: 'prev,next today',
-				center: 'title',
-				right: 'month,agendaWeek,agendaDay'
-			},
-			defaultDate: new Date(),
-			editable: false,
-			selectable: true,
-			selectHelper: true,
-			businessHours : {
-				start: '08:00',
-		    end: '21:00',
-		    dow: [ 1, 2, 3, 4, 5, 6 ]
-			},
-			allDaySlot: false,
-			events: '/agenda/data',
-			dayClick: function(date, jsEvent, view) {
-				if(view.name == 'month' || view.name == 'agendaWeek') {
-					$('#calendar').fullCalendar('changeView', 'agendaDay');
-					$('#calendar').fullCalendar('gotoDate', date);
-				}
-			},
-			eventClick: function( event, jsEvent, view ) {
-				window.location.href = "agenda/" + event.slug;
-			},
-			select: function( start, end, jsEvent, view ){
-				if( view.name == 'agendaDay' ){
-					$("#hdnStart").val(start);
-					$("#hdnEnd").val(end);
-					$('#myModal').modal('toggle');
-				}
+	$('#calendar').fullCalendar({
+		header: {
+			left: 'prev,next today',
+			center: 'title',
+			right: 'month,agendaWeek,agendaDay'
+		},
+		defaultDate: new Date(),
+		editable: false,
+		selectable: true,
+		selectHelper: true,
+		selectConstraint: 'businessHours',
+        	eventConstraint: 'businessHours',
+		businessHours : {
+			start: '08:00',
+			end: '21:00',
+			dow: [ 1, 2, 3, 4, 5, 6 ]
+		},
+		allDaySlot: false,
+		events: '/agenda/data',
+		dayClick: function(date, jsEvent, view) {
+			if(view.name == 'month' || view.name == 'agendaWeek') {
+				$('#calendar').fullCalendar('changeView', 'agendaDay');
+				$('#calendar').fullCalendar('gotoDate', date);
 			}
-		});
-
+		},
+		eventClick: function( event, jsEvent, view ) {
+			window.location.href = "agenda/" + event.slug;
+		},
+		select: function( start, end, jsEvent, view ){
+			if( view.name == 'agendaDay' ){
+				$("#hdnStart").val(start);
+				$("#hdnEnd").val(end);
+				$('#myModal').modal('toggle');
+			}
+		}
 	});
+
+});
